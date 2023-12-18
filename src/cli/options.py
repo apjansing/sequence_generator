7dd6e8a99833118db9659d9bc08b4e10a963a7e3
Alex Jansing <apjansing@gmail.com>
2023-12-18 17:03:22 -0500

    Add ability to save sequence to file

--- src/cli/options.py
+++ src/cli/options.py
@@ -28,3 +28,17 @@ nums = click.option(
     type=int,
     help="Sequence initiators",
 )
+filename = click.option(
+    "-f",
+    "--filename",
+    default=None,
+    type=str,
+    help="Base filename for output files",
+)
+continue_sequence = click.option(
+    "-c",
+    "--continue-sequence",
+    is_flag=True,
+    default=False,
+    help="(TODO)Flag to continue a sequence from a file. Also lets the SequenceGenerator know to append to the file instead of overwriting it.",
+)
--- src/sequence_generator.py
+++ src/sequence_generator.py
@@ -1,5 +1,32 @@
 from sympy import isprime
 from typing import List
+from loguru import logger
+import pandas as pd
+from os import path
+
+
+def write_sequence(func):
+    """
+    Writes or appends a sequence to a file if a filename is provided
+    :param sequence: The sequence to write
+    """
+
+    def wrapper(self, *args, **kwargs):
+        sequence = func(self, *args, **kwargs)
+        self.sequence = sequence
+        if self.filename:
+            if path.exists(f"{self.filename}.csv") and not self.continue_sequence:
+                logger.warning(
+                    f"File {self.filename}.csv already exists. Overwriting file"
+                )
+            pd.DataFrame(sequence).to_csv(
+                f"{self.filename}.csv",
+                mode="a" if self.continue_sequence else "w",
+                header=False,
+            )
+        return sequence
+
+    return wrapper
 
 
 class SequenceGenerator:
@@ -13,21 +40,78 @@ class SequenceGenerator:
         self,
         initial_values: List = [1, 1],
         generator=(lambda x, y: x + y),
+        filename="sequence",
+        continue_sequence: bool = False,
     ):
         self.initial_values = initial_values
-        self.generator = generator
+        self.generator = generator if generator else (lambda x, y: x + y)
+        self.filename = filename
+        self.continue_sequence = continue_sequence
+        self.sequence = []
+        self.value_type = type(self.initial_values[0])
 
+    def get_sequence(self, tail: int = -1):
+        """
+        Reads a sequence from a file if a filename is provided
+        :return: The sequence
+        """
+
+        def get_num_lines(fname):
+            i = 0
+            with open(fname, "r", encoding="UTF-8") as f:
+                for i, _ in enumerate(f):
+                    pass
+            return i + 1
+
+        if self.filename and path.exists(f"{self.filename}.csv"):
+            num_lines = get_num_lines(f"{self.filename}.csv")
+            with open(f"{self.filename}.csv", "r", encoding="UTF-8") as f:
+                if tail > 0:
+                    seq = pd.read_csv(
+                        f, header=None, skiprows=range(1, num_lines - tail)
+                    )
+                seq = pd.read_csv(f, header=None)
+                logger.info(seq)
+                return seq[1].tolist()
+        else:
+            return self.sequence
+
+    def print_sequence(self):
+        """
+        Prints a sequence to the console from a file if a filename is provided,
+        otherwise
+        """
+        sequence: pd.DataFrame = self.get_sequence()
+        if sequence:
+            logger.info(sequence)
+        else:
+            logger.warning("No sequence has been generated yet")
+            logger.info(sequence)
+
+    def generate_sequence_(self, n):
+        """
+        An alternate method that generates a fibonacci-like sequence of numbers,
+        but does not use the write_sequence decorator
+        :param n: The number of numbers to generate
+        :return: A list of numbers
+        """
+        sequence = self.initial_values.copy()
+        for _ in range(n - len(self.initial_values)):
+            # Generate the next number in the sequence by applying the generator
+            # to the last n numbers where n is the length of initial values
+            sequence.append(self.generator(*sequence[-len(self.initial_values) :]))
+        return sequence
+
+    @write_sequence
     def generate_sequence(self, n):
         """
         Generates a fibonacci-like sequence of numbers
         :param n: The number of numbers to generate
         :return: A list of numbers
         """
-        sequence = self.initial_values.copy()
-        for _ in range(len(self.initial_values), n):
-            sequence.append(self.generator(*sequence[-len(self.initial_values) :]))
-        return sequence
+        return self.generate_sequence_(n)
 
+    @write_sequence
     def generate_prime_index_sequence(self, n: int, include_indices: bool = False):
         """
         Generates a fibonacci-like sequence of numbers
@@ -35,18 +119,19 @@ class SequenceGenerator:
         :param include_indices: Whether to include indices in the sequence
         :return: A list of numbers where the indices are prime numbers
         """
-        sequence = self.generate_sequence(n)
+        sequence: pd.DataFrame = self.generate_sequence_(n)
         prime_sequence = []
-        for i, num in enumerate(sequence):
-            if isprime(i + 1):
+        for idx, num in enumerate(sequence):
+            if isprime(idx + 1):
                 if include_indices:
-                    prime_sequence.append((i + 1, num))
+                    prime_sequence.append((idx + 1, num))
                 else:
                     prime_sequence.append(num)
             if len(prime_sequence) == n:
                 break
         return prime_sequence
 
+    @write_sequence
     def generate_prime_sequence(self, n: int, include_indices: bool = False):
         """
         Generates a sequence of prime numbers based on the initial values and generator
@@ -54,7 +139,7 @@ class SequenceGenerator:
         :param include_indices: Whether to include indices in the sequence
         :return: A list of numbers where the indices are prime numbers
         """
-        sequence = self.generate_sequence(n)
+        sequence = self.generate_sequence_(n)
         prime_sequence = []
         for idx, num in enumerate(sequence):
             if isprime(num):
