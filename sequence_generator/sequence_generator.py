48978a75ba0c206b612cde4082360547b06be19e
Alex Jansing <apjansing@gmail.com>
2024-10-13 12:32:27 -0400

    Implement timeout. Note in the fibonacci.py example, we try to generate 100,000 values, but set a timeout of 3 seconds (locally, this file only generates 18,969 values)

--- sequence_generator/sequence_generator.py
+++ sequence_generator/sequence_generator.py
@@ -1,14 +1,13 @@
-from typing import List, Callable, Union
+from typing import Generator, List, Callable, Optional, Union
 import os
 import dill
+import time
 from loguru import logger
 
 
 class SequenceGenerator:
     """
-    This class generates a fibonacci-like sequence
-    :param initial_values: The first n numbers in the sequence
-    :param generator: A function that performs an operation on the last n numbers in the sequence
+    This class to generate sequences of values based on a generator function
     """
 
     def __init__(
@@ -19,6 +18,13 @@ class SequenceGenerator:
         continue_sequence: bool = False,
         memory_safe_mode: bool = False,
     ):
+        '''
+        :param initial_values: The first n values in the sequence
+        :param generator: A function that performs an operation on the last n values in the sequence
+        :param filename: Base filename for the sequence files. This will be used to create a .csv and .pickle file
+        :param continue_sequence: If True, the sequence will be continued from the values in the .csv file
+        :param memory_safe_mode: If True, the sequence will be written to the .csv file after each iteration
+        '''
         if continue_sequence and os.path.exists(f"{filename}.pickle"):
 
             with open(f"{filename}.pickle", "rb") as f:
@@ -28,13 +34,12 @@ class SequenceGenerator:
             self.continue_sequence = continue_sequence
             self.initial_values = initial_values
             self.generator = generator if generator else (lambda x, y: x + y)
-            self.filename = filename if filename else "sequence"
             self.sequence = initial_values.copy()
             self.memory_safe_mode = memory_safe_mode
             self.initial_run = True
-
-        self.csv_path = os.path.join('.', f"{self.filename}.csv")
-        self.pickle_path = os.path.join('.', f"{self.filename}.pickle")
+            filename = filename if filename else "sequence"
+            self.csv_path = os.path.join('.', f"{filename}.csv")
+            self.pickle_path = os.path.join('.', f"{filename}.pickle")
 
         if self.initial_run:
             self.__write_sequence(self.sequence)
@@ -59,7 +64,7 @@ class SequenceGenerator:
         pickle_exists = os.path.exists(self.pickle_path)
         if self.initial_run and (csv_exists or pickle_exists):
             raise FileExistsError(
-                f'''Cannot overwrite existing files on initial run. Please check for {self.filename}.csv and {self.filename}.pickle
+                f'''Cannot overwrite existing files on initial run. Please check for {self.csv_path} and {self.pickle_path}
                 and either delete them or set continue_sequence to True
                 '''
             )
@@ -77,8 +82,8 @@ class SequenceGenerator:
                 f.write(str(s) + "\n")
 
         if pickle_exists and not self.continue_sequence:
-            logger.warning(f"File {self.filename}.pickle already exists. Overwriting file")
-        with open(f"{self.filename}.pickle", "wb") as f:
+            logger.warning(f"File {self.pickle_path} already exists. Overwriting file")
+        with open(self.pickle_path, "wb") as f:
             self.initial_run = False
             f.write(dill.dumps(self))
 
@@ -89,33 +94,57 @@ class SequenceGenerator:
         """
         return self.sequence
 
+    def get_sequence_from_file(self, filename: Optional[str] = None) -> List:
+        '''
+        Reads a sequence from a file
+        :param filename: The name of the file to read
+        :return: The sequence
+        '''
+        filename = filename if filename else self.csv_path
+        with open(filename, "r", encoding="UTF-8") as f:
+            return f.read().splitlines()
+
+    def yield_sequence_from_file(self, filename: Optional[str] = None) -> Generator:
+        '''
+        Reads a sequence from a file - useful for large sequences
+        :param filename: The name of the file to read
+        :return: A generator that yields each line in the file
+        '''
+        filename = filename if filename else self.csv_path
+        with open(filename, "r", encoding="UTF-8") as f:
+            for line in f:
+                yield line.strip()
+
     def print_sequence(self) -> None:
         """
-        Prints a sequence to the console from a file if a filename is provided,
-        otherwise
+        Prints a sequence from the `self.csv_path` file if it exists
+        otherwise prints the `self.sequence` attribute
         """
-        sequence = self.get_sequence()
-        if sequence:
-            logger.info(sequence)
+        if os.path.exists(self.csv_path):
+            sequence = self.yield_sequence_from_file(self.csv_path)
+            for next_value in sequence:
+                logger.info(next_value)
         else:
-            logger.warning("No sequence has been generated yet")
-            logger.info(sequence)
+            sequence = self.sequence
 
-    def generate_sequence_(self, length: int = 1) -> List:
+    def generate_sequence_(self, length: int = 1, timeout: Optional[int] = None) -> List:
         """
-        An alternate method that generates a fibonacci-like sequence of numbers,
+        An alternate method that generates a sequence of values,
         but does not use the write_sequence decorator
-        :param length: The number of numbers to generate
-        :return: A list of numbers
+        :param length: The number of values to generate
+        :param timeout: The number of seconds to wait before timing out
+        :return: A list of values
         """
         to_pick = len(self.initial_values)
-
+        start_time = time.time()
         if self.continue_sequence:
             sequence = []
             for _ in range(length):
-                logger.trace(f"Generating numbers: {self.sequence}")
-                # Generate the next number in the sequence by applying the generator
-                # to the last `length` of numbers where length is the length of initial values
+                if timeout and time.time() - start_time > timeout:
+                    break
+                logger.trace(f"Generating values: {self.sequence}")
+                # Generate the next value in the sequence by applying the generator
+                # to the last `length` of values where length is the length of initial values
                 sequence.append(self.generator(*self.sequence))
                 self.sequence = (
                     sequence[-to_pick:]
@@ -125,23 +154,37 @@ class SequenceGenerator:
         else:
             sequence = self.initial_values.copy()
             for _ in range(length - to_pick):
+                if timeout and time.time() - start_time > timeout:
+                    break
                 # Generate the next number in the sequence by applying the generator
-                # to the last n numbers where n is the length of initial values
+                # to the last n values where n is the length of initial values
                 sequence.append(self.generator(*sequence[-to_pick:]))
 
         return sequence
 
-    def generate_sequence(self, length: int) -> List:
+    def generate_sequence(
+        self,
+        length: Optional[int] = None,
+        memory_safe_mode: bool = False,
+        timeout: Optional[int] = None,
+    ) -> List:
         """
-        Generates a fibonacci-like sequence of numbers
-        :param n: The number of numbers to generate
-        :return: A list of numbers
+        Generates a sequence of values
+        :param length: The number of values to generate
+        :param timeout: The number of seconds to wait before timing out
+        :param memory_safe_mode: If True, the sequence will be written to the .csv file after each iteration
+        :return: A list of values
         """
-        sequence = []
-        if self.memory_safe_mode:
-            for _ in range(length):
-                next_value = self.generate_sequence_()[0]
-                self.__write_sequence(next_value)
-        else:
-            sequence = self.generate_sequence_(length)
+
+        def iterate_sequence(length: int = 1, timeout: Optional[int] = None):
+            sequence = self.generate_sequence_(length=length, timeout=timeout)
             self.__write_sequence(sequence)
+
+        if length:
+            if memory_safe_mode:
+                for _ in range(length):
+                    iterate_sequence(timeout=timeout)
+            else:
+                iterate_sequence(length=length, timeout=timeout)
+        else:
+            iterate_sequence(timeout=timeout)
